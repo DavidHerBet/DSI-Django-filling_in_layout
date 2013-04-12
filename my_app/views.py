80f7787692e7038ae0a96b00d07732d11ea1f1a5
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-04-12 18:46:48 +0100

    Commit inicial

---
+++ my_app/views.py
@@ -0,0 +1,14 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+
+def index(request):
+	return render_to_response('index.html')
+
+def home(request):
+	return render_to_response('home.html')
+
+def help(request):
+	return render_to_response('help.html')
+
+def about(request):
+	return render_to_response('about.html')
