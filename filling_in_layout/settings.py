b4dd780f443fc6b4219840cd515963e9a0e7bb8c
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-04-13 11:03:36 +0100

    Añadir links y estilos a la aplicación

--- filling_in_layout/settings.py
+++ filling_in_layout/settings.py
@@ -112,7 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/alu4078/casa/proyectos-django/filling_in_layout/templates',
+    '/home/alu4078/casa/proyectos-django/filling_in_layout/filling_in_layout/templates',
     '/home/alu4078/casa/proyectos-django/filling_in_layout/my_app/templates',
 )
 
